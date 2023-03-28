9e229a93d25feaf1b875727ad401c55304de5bac
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-03-28 10:24:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace anononymous
+{
+    delegate void mathOperation(int a, int b);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //multicasting delegates ==> incase of void
+            mathOperation d1 = add;
+            mathOperation d2 = sub;
+            mathOperation d3 = mult;
+            mathOperation d5= d1+d2+d3;
+
+            Delegate[] arr=d5.GetInvocationList();  
+            d5.Invoke(2, 3);
+
+        }
+
+        public static void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+
+        }
+        public static void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public static void mult(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+    }
+}
